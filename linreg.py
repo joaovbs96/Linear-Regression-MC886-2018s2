8bdf8ed8ab03a9690e05ea7b060b1db457be4f74
João Vítor Silva <joaovbs96@gmail.com>
2018-08-31 20:52:50 -0300

    Updated linreg.py

--- linreg.py
+++ linreg.py
@@ -6,38 +6,20 @@
 
 import sys
 import numpy as np
-from matplotlib import pyplot as plt
+import pandas as pd
 from sklearn import linear_model
-
-# function to normalize data
-def normalize(data, n):
-    for col in range(1, n):
-        max = float(data[:,col].max())
-        min = float(data[:,col].min())
-        if (max == min):
-            max = min + 1
-        
-        diff = float(max - min)
-        data[:,col] = (data[:,col] - min) / diff
-        
-    return data, min, diff
-
-#function to normalize data
-def normalizeValidation(data, n, min, diff):
-    for col in range(1, n):
-        data[:,col] = (data[:,col] - min) / diff
-        
-    return data
+from matplotlib import pyplot as plt
+from sklearn.metrics import mean_squared_error
 
 # gradient descent
-def gradientDescent(x, y, theta, alpha, m, n, it):
+def gradientDescent(x, y, alpha, n, m, it):
     xTran = x.transpose()
-
+    theta = np.ones(n, dtype=float)
     J = np.zeros(it)
 
     for i in range(it):
         hyp = np.dot(x, theta)
-        loss = np.squeeze(hyp - y)
+        loss = hyp - y['price'].values
         J[i] = (np.sum(loss**2)/(2*m))
         gradient = np.squeeze(np.dot(xTran, loss))/m
         theta = np.squeeze(theta - alpha * gradient)
@@ -46,95 +28,83 @@ def gradientDescent(x, y, theta, alpha, m, n, it):
 
 def normalEquation(x, y):
     inverse = np.linalg.inv(np.dot(x.T, x))
-    thetas = np.dot(np.dot(inverse, x.T), y)
+    thetas = np.dot(np.dot(inverse, x.T), y['price'].values)
     return thetas
-
 
 def calcMAE(x, y, theta, m):
 
     hypothesis = np.dot(x, theta)
     MAE_validation = np.sum(abs(hypothesis - y)) / m
-    
+
     return MAE_validation
 
 ## MAIN
 
+# disable SettingWithCopyWarning warnings
+pd.options.mode.chained_assignment = None  # default='warn'
+
 # Read database
 filename = sys.argv[1]
-file = open(filename, "r")
-line = file.readline()
+data = pd.read_csv(filename)
 
-# Number of features
-n = len(line.split(",")[1:]) + 1
+# Map values of non-numerical features
+cutValue = {'Fair': 1, 'Good': 2, 'Very Good': 3, 'Premium': 4, 'Ideal': 5}
+colorValue = {'J': 1, 'I': 2, 'H': 3, 'G': 4, 'F': 5, 'E': 6, 'D': 7}
+clarityValue = {'I1': 1, 'SI2': 2, 'SI1': 3, 'VS2': 4, 'VS1': 5, 'VVS2': 6, 'VVS1': 7, 'IF': 8}
 
-# Value dictionaries for non-numerical features
-cutValue = {}
-cutValue["Fair"] = 1
-cutValue["Good"] = 2
-cutValue["Very Good"] = 3
-cutValue["Premium"] = 4
-cutValue["Ideal"] = 5
-colorValue = dict(J = 1, I = 2, H = 3, G = 4, F = 5, E = 6, D = 7)
-clarityValue = dict(I1 = 1, SI2 = 2, SI1 = 3, VS2 = 4, VS1 = 5, VVS2 = 6, VVS1 = 7, IF = 8)
+data['color'] = data['color'].map(colorValue)
+data['cut'] = data['cut'].map(cutValue)
+data['clarity'] = data['clarity'].map(clarityValue)
 
-data, price = [], []
-for line in file:
-    line = line.split(",")
+# separate target from data sets
+target = data.drop(data.columns.values[:-1], axis='columns')
+data = data.drop('price', axis='columns')
 
-    temp = []
-    temp.append(1)
-    temp.append(float(line[0]))
-    temp.append(float(cutValue[line[1].replace(chr(34), "")]))
-    temp.append(float(colorValue[line[2].replace(chr(34), "")]))
-    temp.append(float(clarityValue[line[3].replace(chr(34), "")]))
-    temp.append(float(line[4]))
-    temp.append(float(line[5]))
-    temp.append(float(line[6]))
-    temp.append(float(line[7]))
-    temp.append(float(line[8]))
-    data.append(temp)
+# split dataset into train and validation
+trainData, trainTarget = data.iloc[8091:], target.iloc[8091:]
+validData, validTarget = data.iloc[:8091], target.iloc[:8091]
 
-    price.append(float(line[9]))
+# normalize features
+for c in trainData.columns.values:
+    max = float(trainData[c].max())
+    min = float(trainData[c].min())
 
-# split training/test
-validationData, trainingData = [], []
+    if (max == min):
+        max = min + 1
 
-validationData = data[-8091:]
-trainingData = data[:-8091]
-validationPrice = price[-8091:] 
-trainingPrice = price[:-8091]
+    diff = float(max - min)
 
-validationData, min, diff = normalize(np.array(validationData), n)
-trainingData = normalizeValidation(np.array(trainingData), n, min, diff)
+    trainData[c] -= min
+    trainData[c]  /= diff
 
-validationPrice = np.array(validationPrice)
-trainingPrice = np.array(trainingPrice)
+    validData[c] -= min
+    validData[c] /= diff
 
-m = len(trainingPrice)
+# TODO: save min and diff of each column for posterior use with test data(?)
 
-# cost
-# alphaMax = 0.00000001
-alpha = 0.00000001
-# itMax = 100000
-it = 1000
-thetasGD = np.ones(n, dtype=float)
-J, thetGD = gradientDescent(trainingData, trainingPrice, thetasGD, alpha, m, n, it)
+# calculate cost
+m, n = trainData.shape
+it = 10000 # itMax = 100000
+alpha = 0.01 # alphaMax = 0.00000001
 
-samplesNumber, _ = np.shape(validationData)
-#T = [0.95526007, 0.99452856, 0.96676548, 0.97391538, 0.98008266, 0.97620609, 0.99565883, 0.98043281, 0.97619554, 0.98760811]
-MAE_test_reg_GD = calcMAE(validationData, validationPrice, thetasGD, samplesNumber)
-print(MAE_test_reg_GD)
+# apply gradient descent
+J, thetas = gradientDescent(trainData, trainTarget, alpha, n, m, it)
 
-#plt.plot(J)
-#plt.show()
+plt.plot(J)
+plt.show()
 
 clf = linear_model.SGDRegressor(max_iter=it, eta0=alpha, learning_rate='constant')
-clf.fit(trainingData, trainingPrice)
+clf.fit(trainData, trainTarget['price'].values)
 
-MAE_test_reg_GD = calcMAE(validationData, validationPrice, clf.coef_, samplesNumber)
-print(MAE_test_reg_GD)
+thetasNE = normalEquation(trainData, trainTarget)
 
-thetasNE = normalEquation(trainingData, trainingPrice)
-T = [ 0.06855487, 2.81424015, 0.02465473, 0.10490848, 0.19005936, -0.53677227, 0.15405773, -0.00671723, -0.15854809, -0.08902239]
-MAE_test_NE = calcMAE(validationData, validationPrice, thetasNE, samplesNumber)
-print(MAE_test_NE)
+y = trainTarget['price'].values.ravel()
+print('MSE')
+print('Our Model: ' + str(mean_squared_error(np.dot(trainData, thetas), y)))
+print('SKLearn: ' + str(mean_squared_error(np.dot(trainData, clf.coef_), y)))
+print('Normal Equation: ' + str(mean_squared_error(np.dot(trainData, thetasNE), y)))
+
+print('MAE')
+print('Our Model: ' + str(calcMAE(trainData, y, thetas, m)))
+print('SKLearn: ' + str(calcMAE(trainData, y, clf.coef_, m)))
+print('Normal Equation: ' + str(calcMAE(trainData, y, thetasNE, m)))
